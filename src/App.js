58daf4e2d60b429f1791e594a9fb1ae622e31662
Wahab Khan <kwahab789@gmail.com>
2021-12-03 16:36:28 +0500

    search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Axios from "axios";
+import { useState } from "react";
 function App() {
+  const [query, setquery] = useState("");
   const YOUR_APP_ID = "3b620bbf";
   const YOUR_APP_KEY = "bd121d42f201323d54f9cdd1229c29e6";
 
-  var url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`;
+  var url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&&health=alcohol-free`;
   const getRecipes = async () => {
     var result = await Axios.get(url);
     console.log(result.data);
@@ -14,6 +16,16 @@ function App() {
       <p className="title" onClick={getRecipes}>
         Food Recipe Plaza 🍔
       </p>
+      <form className="app-searchForm">
+        <input
+          className="app-input"
+          type="text"
+          placeholder="Enter Ingridient"
+          value={query}
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <input className="app-submit" type="submit" value="Search" />
+      </form>
     </div>
   );
 }
